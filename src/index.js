8611cc2cab7e0a4ca52bcf61cd4d46c540faf2d3
Tony Ingle <ingleanthonyco@gmail.com>
2022-03-30 16:50:16 -0600

    deployed to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.render(
-	<React.StrictMode>
+	<HashRouter basename='/'>
 		<App />
-	</React.StrictMode>,
+	</HashRouter>,
 	document.getElementById('root')
 );
