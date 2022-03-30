cd1974d76f70e1b15c258fa4b16a05558766e483
Tony Ingle <ingleanthonyco@gmail.com>
2022-03-30 16:32:19 -0600

    small styling changes to desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,15 @@ import React from 'react';
 import logo from '../images/logo.svg';
 
 const Header = () => {
+	const showNav = () => {
+		const menuBtn = document.querySelector('.menu-btn');
+		const navbar = document.querySelector('nav');
+
+		menuBtn.addEventListener('click', () => {
+			navbar.classList.toggle('open');
+			menuBtn.classList.toggle('rotate');
+		});
+	};
 	return (
 		<>
 			<header className='header'>
@@ -9,6 +18,9 @@ const Header = () => {
 					<img src={logo} alt='LoopStudios' />
 				</div>
 				<nav>
+					<div className='nav-logo'>
+						<img src={logo} alt='logo' />
+					</div>
 					<ul>
 						<li>
 							<button>About</button>
@@ -27,7 +39,7 @@ const Header = () => {
 						</li>
 					</ul>
 				</nav>
-				<div className='menu-btn'>
+				<div className='menu-btn' onClick={() => showNav()}>
 					<div className='bar bar-1'></div>
 					<div className='bar bar-2'></div>
 					<div className='bar bar-3'></div>
